f2cd8f76167cafb4545de3929f860d2cfb2cd416
tina14172 <thummartina149@gmail.com>
2026-06-26 11:24:06 +0530

    array oobject function complate

--- 9.string/string1.js
+++ 9.string/string1.js
@@ -18,5 +18,31 @@ var age = 12 ;
 
 // your name is raj . your age is 12.
 
-console.log("your name is " , fname , " . your age is " , age , " . ");
+// console.log("your name is " , fname , " . your age is " , age , " . ");
+console.log('your name is ' , fname , '. your age is ' , age , '.');
+// // String Formating : 
+// console.log(`Your name is ${fname}. your age is ${age}.`);
+// console.log('Your name is ${fname}. your age is ${age}.');  // wrong 
+// console.log("Your name is ${fname}. your age is ${age}."); // wrong 
 
+// console.log(`This 
+//     is a New 
+//     Line`);
+
+
+var str = "This is NewData.";
+console.log(str);
+
+// 1. length : proprty  
+// console.log(str.length);
+
+// method : 
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+
+// console.log(str.includes("ew"));
+
+console.log(str.split());
+console.log(str.split(""));
+console.log(str.split(" "));
